ea92d9c3f811efe0a65c058daabc3273b108ccd2
Tanay Mathur <tanay.mathur94@gmail.com>
2016-01-15 03:08:09 +0530

    Adding all files.

---
+++ polls/models.py
@@ -0,0 +1,24 @@
+from __future__ import unicode_literals
+
+import datetime
+
+from django.db import models
+from django.utils import timezone
+
+class Question(models.Model):
+
+	def __str__(self):
+		return self.question_text
+
+	def was_published_recently(self):
+		return timezone.now() - datetime.timedelta(days=1) <= self.pub_date <= timezone.now()
+	
+	question_text = models.CharField(max_length=200)
+	pub_date = models.DateTimeField('date published')
+
+class Choice(models.Model):
+	def __str__(self):
+		return self.choice_text
+	question = models.ForeignKey(Question, on_delete=models.CASCADE)
+	choice_text = models.CharField(max_length=200)
+	votes = models.IntegerField(default=0)
